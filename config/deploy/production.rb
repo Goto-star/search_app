12f3d48c4435fc61fea147328ae16f426f9905d9
Goto-star <goyu620@gmail.com>
2023-03-19 14:37:08 +0900

    circleCIのproduction.rbの変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,6 @@
 server '54.238.206.240', user: 'ec2-user', roles: %w[app db web]
 
 set :ssh_options, {
-  port: 20,
   keys: [File.expand_path('~/.ssh/id_rsa')],
   forward_agent: true,
   auth_methods: %w[publickey]
